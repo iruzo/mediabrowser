0e1fea3ece1fc7287e7066c6a6f1a4c287ad5da7
amnesia <amnesia>
2026-02-21 18:07:39 +0000

    refactor(ui): embed static directory with include_dir
    - replace hardcoded per-file UI routes with dynamic /ui/* asset lookup
    - serve embedded files with mime-based content type and index fallback
    - add include_dir dependency with default features disabled

--- src/endpoints/ui.rs
+++ src/endpoints/ui.rs
@@ -1,209 +1,56 @@
+use include_dir::{include_dir, Dir};
+use mime_guess::from_path;
+use std::convert::Infallible;
+use warp::http::StatusCode;
+use warp::hyper::Body;
 use warp::Filter;
 use warp::Reply;
 
-fn text_response(content: &'static str, content_type: &'static str) -> warp::reply::Response {
-    warp::reply::with_header(content, "content-type", content_type).into_response()
+static UI_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/static");
+
+fn index_response() -> warp::reply::Response {
+    match UI_DIR.get_file("index.html") {
+        Some(file) => warp::http::Response::builder()
+            .status(StatusCode::OK)
+            .header("content-type", "text/html; charset=utf-8")
+            .body(Body::from(file.contents()))
+            .unwrap(),
+        None => warp::reply::with_status("Missing index.html", StatusCode::INTERNAL_SERVER_ERROR)
+            .into_response(),
+    }
+}
+
+fn file_response(path: &str) -> warp::reply::Response {
+    match UI_DIR.get_file(path) {
+        Some(file) => {
+            let content_type = from_path(path).first_or_octet_stream().to_string();
+            warp::http::Response::builder()
+                .status(StatusCode::OK)
+                .header("content-type", content_type)
+                .body(Body::from(file.contents()))
+                .unwrap()
+        }
+        None => index_response(),
+    }
+}
+
+async fn serve_ui_path(tail: warp::path::Tail) -> Result<warp::reply::Response, Infallible> {
+    let path = tail.as_str().trim_start_matches('/');
+    if path.is_empty() {
+        return Ok(index_response());
+    }
+
+    Ok(file_response(path))
 }
 
 pub fn ui_routes() -> warp::filters::BoxedFilter<(warp::reply::Response,)> {
-    let ui_index = warp::path("ui").and(warp::path::end()).map(|| {
-        warp::reply::html(include_str!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/static/index.html"
-        )))
-        .into_response()
-    });
+    let ui_index = warp::path("ui")
+        .and(warp::path::end())
+        .map(index_response);
 
-    let ui_css_base = warp::path("ui")
-        .and(warp::path("css"))
-        .and(warp::path("base.css"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/css/base.css")),
-                "text/css",
-            )
-        });
+    let ui_path = warp::path("ui")
+        .and(warp::path::tail())
+        .and_then(serve_ui_path);
 
-    let ui_css_layout = warp::path("ui")
-        .and(warp::path("css"))
-        .and(warp::path("layout.css"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/css/layout.css")),
-                "text/css",
-            )
-        });
-
-    let ui_css_components = warp::path("ui")
-        .and(warp::path("css"))
-        .and(warp::path("components.css"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(
-                    env!("CARGO_MANIFEST_DIR"),
-                    "/static/css/components.css"
-                )),
-                "text/css",
-            )
-        });
-
-    let ui_css_viewer = warp::path("ui")
-        .and(warp::path("css"))
-        .and(warp::path("viewer.css"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/css/viewer.css")),
-                "text/css",
-            )
-        });
-
-    let ui_css_responsive = warp::path("ui")
-        .and(warp::path("css"))
-        .and(warp::path("responsive.css"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(
-                    env!("CARGO_MANIFEST_DIR"),
-                    "/static/css/responsive.css"
-                )),
-                "text/css",
-            )
-        });
-
-    let ui_js_state = warp::path("ui")
-        .and(warp::path("js"))
-        .and(warp::path("state.js"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/js/state.js")),
-                "application/javascript",
-            )
-        });
-
-    let ui_js_utils = warp::path("ui")
-        .and(warp::path("js"))
-        .and(warp::path("utils.js"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/js/utils.js")),
-                "application/javascript",
-            )
-        });
-
-    let ui_js_navigation = warp::path("ui")
-        .and(warp::path("js"))
-        .and(warp::path("navigation.js"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/js/navigation.js")),
-                "application/javascript",
-            )
-        });
-
-    let ui_js_gallery = warp::path("ui")
-        .and(warp::path("js"))
-        .and(warp::path("gallery.js"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/js/gallery.js")),
-                "application/javascript",
-            )
-        });
-
-    let ui_js_selection = warp::path("ui")
-        .and(warp::path("js"))
-        .and(warp::path("selection.js"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/js/selection.js")),
-                "application/javascript",
-            )
-        });
-
-    let ui_js_media_viewer = warp::path("ui")
-        .and(warp::path("js"))
-        .and(warp::path("media-viewer.js"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(
-                    env!("CARGO_MANIFEST_DIR"),
-                    "/static/js/media-viewer.js"
-                )),
-                "application/javascript",
-            )
-        });
-
-    let ui_js_media_controls = warp::path("ui")
-        .and(warp::path("js"))
-        .and(warp::path("media-controls.js"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(
-                    env!("CARGO_MANIFEST_DIR"),
-                    "/static/js/media-controls.js"
-                )),
-                "application/javascript",
-            )
-        });
-
-    let ui_js_file_ops = warp::path("ui")
-        .and(warp::path("js"))
-        .and(warp::path("file-ops.js"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/js/file-ops.js")),
-                "application/javascript",
-            )
-        });
-
-    let ui_js_main = warp::path("ui")
-        .and(warp::path("js"))
-        .and(warp::path("main.js"))
-        .map(|| {
-            text_response(
-                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/js/main.js")),
-                "application/javascript",
-            )
-        });
-
-    let ui_fallback = warp::path("ui").and(warp::path::tail()).map(|_tail: warp::path::Tail| {
-        warp::reply::html(include_str!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/static/index.html"
-        )))
-        .into_response()
-    });
-
-    ui_index
-        .or(ui_css_base)
-        .unify()
-        .or(ui_css_layout)
-        .unify()
-        .or(ui_css_components)
-        .unify()
-        .or(ui_css_viewer)
-        .unify()
-        .or(ui_css_responsive)
-        .unify()
-        .or(ui_js_state)
-        .unify()
-        .or(ui_js_utils)
-        .unify()
-        .or(ui_js_navigation)
-        .unify()
-        .or(ui_js_gallery)
-        .unify()
-        .or(ui_js_selection)
-        .unify()
-        .or(ui_js_media_viewer)
-        .unify()
-        .or(ui_js_media_controls)
-        .unify()
-        .or(ui_js_file_ops)
-        .unify()
-        .or(ui_js_main)
-        .unify()
-        .or(ui_fallback)
-        .unify()
-        .boxed()
+    ui_index.or(ui_path).unify().boxed()
 }
